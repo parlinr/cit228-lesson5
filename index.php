b93e26fadb888f9476648b1d6f9cd1df7168a13e
Ryan Parlin <parlinr@ad.nmc.edu>
2018-03-11 19:08:15 -0400

    schema finished

--- index.php
+++ index.php
@@ -3,11 +3,13 @@ echo <<<END
 <h1>Database Design</h1>
 <br>
 <p>
-    <em>Employee:</em> employeeId(PK), firstName, lastName, address, city, state, zip
+    <em>Employee:</em> employeeId(PK), firstName, lastName, address, zip(FK -> Location)
     <br>
-    <em>Invoice:</em> invoiceId(PK), employeeId(FK -> Employee), customerId(FK -> Customer),
+    <em>Invoice:</em> invoiceId(PK), employeeId(FK -> Employee), customerId(FK -> Customer), invoiceAmount, terms
     <br>
-    <em>Customer:</em> customerId(PK), name, address, city, state, zip
+    <em>Customer:</em> customerId(PK), name, address, zip(FK -> Location)
+    <br>
+    <em>Location:</em> zip(PK), city, state
 </p>
 
 
